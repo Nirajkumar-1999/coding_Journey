94c3909d898e6224463f888755256c3aa7ccad3e
Kumar <niraj.i.kumar@capgemini.com>
2023-02-14 16:06:12 +0530

    collections

---
+++ CollectionFramework/src/com/practiceCollection/ArrayListFunc.java
@@ -0,0 +1,21 @@
+package com.practiceCollection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListFunc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Integer> al = new ArrayList<Integer> ();
+		
+		al.add(10);
+		al.add(29);
+		al.add(30);
+		
+		System.out.println(al);
+
+	}
+
+}
